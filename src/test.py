1b914fa3d809e9a9372dc7f64d77d6230a84783c
devmatte <mattejonsson@hotmail.se>
2019-02-25 13:21:33 +0100

    Data cleaning and stop-words

--- src/test.py
+++ src/test.py
@@ -1,17 +1,30 @@
-#from sklearn.datasets import fetch_20newsgroups
+from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
-#categories = ['alt.atheism']
-#newsgroups_train = fetch_20newsgroups(subset='train', categories=categories)
+import urllib.request
 
-newsgroups_train = [
-    'This is the first document.',
-    'This is the second second document.',
-    'And the third one.',
-    'Is this the first document?',
-]
-print(newsgroups_train)
 
-# turn the text into vectors of numerical values suitable for statistical analysis
-vectorizer = TfidfVectorizer()
-vectors = vectorizer.fit_transform(newsgroups_train)
-print(vectors)
+# specific categories for testing (faster load time)
+categories = ['alt.atheism', 'talk.religion.misc',
+              'comp.graphics', 'sci.space']
+
+# fetch dataset
+newsgroups_train = fetch_20newsgroups(subset='train',
+                                      remove=('headers', 'footers'),
+                                      categories=categories)
+
+
+# get stopwords from file
+def get_stopwords():
+    f = open('./assets/stopwords.txt')  #https://github.com/suzanv/termprofiling/blob/master/stoplist.txt
+    x = f.read().split("\n")
+    f.close()
+    return x
+
+
+# feature extraction
+vectorizer = TfidfVectorizer(stop_words=get_stopwords())
+vectors = vectorizer.fit_transform(newsgroups_train.data)
+
+#print(vectors[0]) #Display first vector
+print(vectors.toarray()) #Display as vector
+print(len(vectorizer.get_feature_names())) #Display feature count
