7f77bd54a0ee938efa49d9522cc04247fdedad5f
Lucas <LUCBO@users.noreply.github.com>
2019-03-10 19:16:42 +0100

    Lemmatize 20 newsgroups
    Loops and lemmatize. Quite slow may need to save result instead of running each time

--- src/test/Lemmatize.py
+++ src/test/Lemmatize.py
@@ -3,6 +3,7 @@ import nltk
 from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
 import string
+import time
 
 # specific categories for testing (faster load time)
 categories = ['alt.atheism', 'talk.religion.misc',
@@ -13,21 +14,34 @@ newsgroups_train = fetch_20newsgroups(subset='train',
                                       remove=('headers', 'footers'),
                                       categories=categories)
 
+lemmatizer = WordNetLemmatizer()
+
 
 def get_wordnet_pos(word):
-    """Map POS tag to first character lemmatize() accepts"""
     tag = nltk.pos_tag([word])[0][1][0].upper()
     tag_dict = {"J": wordnet.ADJ,
                 "N": wordnet.NOUN,
                 "V": wordnet.VERB,
                 "R": wordnet.ADV}
-
     return tag_dict.get(tag, wordnet.NOUN)
 
 
-lemmatizer = WordNetLemmatizer()
-print([newsgroups_train.data[0]])
-word_list = nltk.word_tokenize(newsgroups_train.data[0])
-newsgroups_train.data[0] = (" ".join([lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in
-                                      nltk.word_tokenize(newsgroups_train.data[0]) if w not in string.punctuation]))
+def lemmatize_newsgroup(newsgroups):
+    i = 0
+    time_before = time.time()
+    while i < len(newsgroups.data):
+        newsgroups.data[i] = (" ".join([lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in
+                                        nltk.word_tokenize(newsgroups.data[i]) if w not in string.punctuation]))
+        print(i)
+        i += 1
+    time_after = time.time()
+    print("Time:", time_after-time_before, " seconds")
+
+
+text = ([newsgroups_train.data[0]])
+text2 = ([newsgroups_train.data[2033]])
+lemmatize_newsgroup(newsgroups_train)
+print(text)
 print(newsgroups_train.data[0])
+print(text2)
+print(newsgroups_train.data[2033])
