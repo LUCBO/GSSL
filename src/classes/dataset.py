4c89c4f70903bad70d2e78c70cb1797c34369f6d
devmatte <mattejonsson@hotmail.se>
2019-03-12 16:02:55 +0100

    Split training dataset into labeled and unlabeled

---
+++ src/classes/dataset.py
@@ -0,0 +1,38 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.model_selection import train_test_split
+
+class Dataset:
+    def __init__(self, categories=None):
+        self.categories = fetch_20newsgroups(subset='train', categories=categories).target_names  # List of categories in dataset
+
+        self.train = {  # Labeled dataset
+            'data': [],
+            'target': []
+        }
+
+        self.test = {  # "Unlabeled" dataset
+            'data': [],
+            'target': []
+        }
+
+    def split(self, category_size):
+        """
+        Split training dataset into train and test for semi-supervised learning
+        :param category_size: Number of random documents per category in training dataset
+        :return:
+        """
+
+        for category in self.categories:
+            fetch_dataset = fetch_20newsgroups(subset='train',
+                                               remove=('headers', 'footers', 'quotes'),
+                                               categories=[category])
+
+            xTrain, xTest, yTrain, yTest = train_test_split(fetch_dataset.data, fetch_dataset.target,
+                                                            train_size=category_size, random_state=0)
+
+            self.train['data'].extend(xTrain)
+            self.train['target'].extend(yTrain)
+            self.test['data'].extend(xTest)
+            self.test['target'].extend(yTest)
+
+
--- src/test.py
+++ src/test.py
@@ -6,19 +6,18 @@ from sklearn import metrics
 import nltk
 from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
+from classes.dataset import Dataset
 
 # specific categories for testing (faster load time)
 categories = ['alt.atheism', 'talk.religion.misc',
               'comp.graphics', 'sci.space']
 
-# fetch dataset
-newsgroups_train = fetch_20newsgroups(subset='train',
-                                      remove=('headers', 'footers'),
-                                      categories=categories)
-
-newsgroups_test = fetch_20newsgroups(subset='test',
-                                     remove=('headers', 'footers'),
-                                     categories=categories)
+# initialize dataset
+dataset = Dataset(categories)
+dataset.split(100)
+print('Traning rows: ', len(dataset.train['data']))
+print('Testing rows: ', len(dataset.test['data']))
+print(dataset.train['data'][1])
 
 
 # get stopwords from file
@@ -45,15 +44,14 @@ lemmatizer = WordNetLemmatizer()
 sentence = "The striped bats are hanging on their feet for best"
 print([lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in nltk.word_tokenize(sentence)])
 
-
 # feature extraction
 vectorizer = TfidfVectorizer(stop_words=get_stopwords())
 lem = WordNetLemmatizer()
-vectors = vectorizer.fit_transform(newsgroups_train.data)
+vectors = vectorizer.fit_transform(dataset.train['data'])
 
 # classification
-clf = LabelPropagation(kernel='rbf', gamma=0.89).fit(vectors.todense(), newsgroups_train.target)
-test_vec = vectorizer.transform(newsgroups_test.data)
+clf = LabelPropagation(kernel='rbf', gamma=0.89).fit(vectors.todense(), dataset.train['target'])
+test_vec = vectorizer.transform(dataset.test['data'])
 pred = clf.predict(test_vec)
-print(metrics.f1_score(newsgroups_test.target, pred, average='macro'))
-print(clf.score(test_vec, newsgroups_test.target))
+print('f1 score: ', metrics.f1_score(dataset.test['target'], pred, average='macro'))
+print('clf score: ', clf.score(test_vec, dataset.test['target']))
