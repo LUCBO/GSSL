52d1f35968d66a664fea67ac470197673d1d1a3d
Lucas <LUCBO@users.noreply.github.com>
2019-03-18 17:01:57 +0100

    Lowercase and split training data by category

--- src/functions/Preprocess.py
+++ src/functions/Preprocess.py
@@ -3,12 +3,24 @@ import nltk
 from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
 import string
+from sklearn.datasets import fetch_20newsgroups
 
 
 # Preprocess the 20 Newsgroups data
-def process(trainingdata, testdata):
-    remove_regex_words(trainingdata)
-    lemmatize_newsgroup(trainingdata, testdata)
+def process(categories):
+    i = 0
+    while i < len(categories):
+        trainingdata = fetch_20newsgroups(subset='train',
+                                          remove=('headers', 'footers', 'quotes'),
+                                          categories=[categories[i]])
+        remove_regex_words(trainingdata)
+        lemmatize_newsgroup_train(trainingdata, categories[i])
+        i += 1
+    testdata = fetch_20newsgroups(subset='test',
+                                  remove=('headers', 'footers', 'quotes'),
+                                  categories=categories)
+    remove_regex_words(testdata)
+    lemmatize_newsgroup_test(testdata)
 
 
 # Determines how a word shall change to convert it to its base form
@@ -21,26 +33,35 @@ def get_wordnet_pos(word):
     return tag_dict.get(tag, wordnet.NOUN)
 
 
-# Lemmatize newsgroups documents
-def lemmatize_newsgroup(newsgroups_train, newsgroups_test):
+# Lemmatize newsgroups training documents
+def lemmatize_newsgroup_train(newsgroups_train, category):
     i = 0
     lemmatizer = WordNetLemmatizer()
     size = len(newsgroups_train.data)
     print("Lemmatization in progress...")
-    print("Training data: ", i, "/", size)
-    with open('../assets/newsgroups_train.txt', 'w') as f:
+    print(category + " training data: ", i, "/", size)
+    with open('../assets/20newsgroups/train/newsgroups_train_' + category + '.txt', 'w') as f:
         while i < len(newsgroups_train.data):
+            newsgroups_train.data[i] = newsgroups_train.data[i].lower()
             newsgroups_train.data[i] = (" ".join([lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in
                                                   nltk.word_tokenize(newsgroups_train.data[i]) if w not in
                                                   string.punctuation]))
             f.write("%s\n" % newsgroups_train.data[i].encode("utf-8"))
             i += 1
-            print("Training data: ", i, "/", size)
+            print(category + " training data: ", i, "/", size)
+    print("Lemmatization finished")
+
+
+# Lemmatize newsgroups test documents
+def lemmatize_newsgroup_test(newsgroups_test):
+    lemmatizer = WordNetLemmatizer()
     i = 0
+    print("Lemmatization in progress...")
     with open('../assets/newsgroups_test.txt', 'w') as f:
         size = len(newsgroups_test.data)
         print("Test data: ", i, "/", size)
         while i < len(newsgroups_test.data):
+            newsgroups_test.data[i] = newsgroups_test.data[i].lower()
             newsgroups_test.data[i] = (" ".join([lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in
                                                  nltk.word_tokenize(newsgroups_test.data[i]) if w not in
                                                  string.punctuation]))
@@ -51,18 +72,24 @@ def lemmatize_newsgroup(newsgroups_train, newsgroups_test):
 
 
 # loads newsgroups from files
-def load_improved_newsgroup(newsgroups_train, newsgroups_test):
-    lines = [line.rstrip('\n') for line in open('./assets/newsgroups_train.txt')]
+def load_improved_newsgroup_train(newsgroups_train, category):
+    lines = [line.rstrip('\n') for line in open('../assets/20newsgroups/train/newsgroups_train_'
+                                                + category + '.txt')]
     i = 0
     size = len(lines)
     print("Training data is loading...")
-    print("Training data: ", i, "/", size)
+    print(category + " training data: ", i, "/", size)
     while i < len(lines):
         length = len(lines[i])
         newsgroups_train['data'][i] = lines[i][1:length]  # Removes the b
         i += 1
-        print("Training data: ", i, "/", size)
-    lines = [line.rstrip('\n') for line in open('./assets/newsgroups_test.txt')]
+        print(category + " training data: ", i, "/", size)
+    print("Training data is loaded")
+
+
+# loads newsgroups test documents from files
+def load_improved_newsgroup_test(newsgroups_test):
+    lines = [line.rstrip('\n') for line in open('../assets/newsgroups_test.txt')]
     i = 0
     size = len(lines)
     print("Test data is loading...")
@@ -72,7 +99,7 @@ def load_improved_newsgroup(newsgroups_train, newsgroups_test):
         newsgroups_test['data'][i] = lines[i][1:length]  # Removes the b
         i += 1
         print("Test data: ", i, "/", size)
-    print("All data is loaded")
+    print("Test data is loaded")
 
 
 # get stopwords from file
--- src/test/TestPreprocess.py
+++ src/test/TestPreprocess.py
@@ -2,26 +2,44 @@ import src.functions.Preprocess
 from sklearn.datasets import fetch_20newsgroups
 from src.classes.dataset import Dataset
 
-categories = ['alt.atheism']
+categories = ['alt.atheism',
+              'comp.graphics',
+              'comp.os.ms-windows.misc',
+              'comp.sys.ibm.pc.hardware',
+              'comp.sys.mac.hardware',
+              'comp.windows.x',
+              'misc.forsale',
+              'rec.autos',
+              'rec.motorcycles',
+              'rec.sport.baseball',
+              'rec.sport.hockey',
+              'sci.crypt',
+              'sci.electronics',
+              'sci.med',
+              'sci.space',
+              'soc.religion.christian',
+              'talk.politics.guns',
+              'talk.politics.mideast',
+              'talk.politics.misc',
+              'talk.religion.misc']
+
+category = ['alt.atheism']
 
 newsgroups_train = fetch_20newsgroups(subset='train',
                                       remove=('headers', 'footers', 'quotes'),
-                                      )
+                                      categories=category)
 newsgroups_test = fetch_20newsgroups(subset='test',
                                      remove=('headers', 'footers', 'quotes'),
-                                     )
+                                     categories=categories)
 
 before = [newsgroups_train.data[0]]
-# src.functions.Preprocess.process(newsgroups_train, newsgroups_test)  # Only first time
+before_testdata = [newsgroups_test.data[0]]
+# src.functions.Preprocess.process(categories)  # Only first time
 
-newsgroups_train = fetch_20newsgroups(subset='train',
-                                      remove=('headers', 'footers', 'quotes'),
-                                      )
-newsgroups_test = fetch_20newsgroups(subset='test',
-                                     remove=('headers', 'footers', 'quotes'),
-                                     )
-
-dataset = Dataset(categories)
-src.functions.Preprocess.load_improved_newsgroup(dataset.train, dataset.test)  # Only thing needed other times
+dataset = Dataset(category)
+src.functions.Preprocess.load_improved_newsgroup_train(dataset.train, 'alt.atheism')  # Only thing needed other times
+src.functions.Preprocess.load_improved_newsgroup_test(newsgroups_test)
 print(before)
 print([dataset.train['data'][0]])
+print(before_testdata)
+print([newsgroups_test.data[0]])
